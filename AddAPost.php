f679136ff53a070eef2d650a32549d4bf407bbb7
Ferree01 <benferree3@gmail.com>
2016-11-28 14:19:48 -0700

    Create AddAPost.php

---
+++ AddAPost.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>
+Add Post
+</title>
+</head>
+<body>
+<form action="addResult.php" method"post">
+Enter Name:<input type="text" name="name"><br>
+Enter Your Post:<input type"text" name="post"><br>
+<input type="submit">
+</body>
+</html>
