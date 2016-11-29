c784271692377c449fc55417d178b12361b69a15
Ferree01 <benferree3@gmail.com>
2016-11-28 17:51:13 -0700

    Update CreateATable.php

--- CreateATable.php
+++ CreateATable.php
@@ -1,1 +1,16 @@
+// sql to create table
+$sql = "CREATE TABLE members (
+memberID INT NOT NULL AUTO_INCREMENT,
+name VARCHAR(128),
+post VARCHAR(128),
+PRIMARY KEY memberID
+)";
 
+if ($conn->query($sql) === TRUE) {
+    echo "Post table created";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+$conn->close();
+?>
